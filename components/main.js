fd3ebb22c6d3a1a7ca12a3fa4688abf01615ebe6
Blackxok <nasridoni1@gmail.com>
2023-02-03 17:41:59 +0500

    start type code 19/00 22/12

--- components/main.js
+++ components/main.js
@@ -15,7 +15,7 @@ new Swiper(".swiper", {
     watchOverflow: true,
 
     // nechta kalonka
-    slidesPerView: 1,
+    slidesPerView: 3,
 
     // kalonkalar orasi
     spaceBetween: 10,
@@ -29,3 +29,35 @@ new Swiper(".swiper", {
         watchState: true,
     },
 });
+// ----------------------------------------------------------------------
+// // swiper slider
+// new Swiper(".swiper_udo", {
+//     // navigation
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+//     // pagination
+//     pagination: {
+//         el: ".swiper-pagination",
+//         // klick
+//         clickable: true,
+//     },
+//     // kalonka mayda bo'sa
+//     watchOverflow: true,
+
+//     // nechta kalonka
+//     slidesPerView: 1,
+
+//     // kalonkalar orasi
+//     spaceBetween: 10,
+
+//     // scroll mishka
+//     // mousewheel: {
+//     //     sesitivity: 3,
+//     // },
+
+//     hashNavigation: {
+//         watchState: true,
+//     },
+// });

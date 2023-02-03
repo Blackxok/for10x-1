c0958800159cba98ba6017018012280521fd7ede
Blackxok <nasridoni1@gmail.com>
2023-02-03 18:47:45 +0500

    fixeded

--- components/main.js
+++ components/main.js
@@ -29,35 +29,36 @@ new Swiper(".swiper", {
         watchState: true,
     },
 });
-// ----------------------------------------------------------------------
-// // swiper slider
-// new Swiper(".swiper_udo", {
-//     // navigation
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//     },
-//     // pagination
-//     pagination: {
-//         el: ".swiper-pagination",
-//         // klick
-//         clickable: true,
-//     },
-//     // kalonka mayda bo'sa
-//     watchOverflow: true,
 
-//     // nechta kalonka
-//     slidesPerView: 1,
+// -----------------------------------------
+// swiper slider
+new Swiper(".swiper_udo", {
+    // navigation
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    // pagination
+    pagination: {
+        el: ".swiper-pagination",
+        // klick
+        clickable: true,
+    },
+    // kalonka mayda bo'sa
+    watchOverflow: true,
 
-//     // kalonkalar orasi
-//     spaceBetween: 10,
+    // nechta kalonka
+    slidesPerView: 1,
 
-//     // scroll mishka
-//     // mousewheel: {
-//     //     sesitivity: 3,
-//     // },
+    // kalonkalar orasi
+    spaceBetween: 10,
 
-//     hashNavigation: {
-//         watchState: true,
-//     },
-// });
+    // scroll mishka
+    // mousewheel: {
+    //     sesitivity: 3,
+    // },
+
+    hashNavigation: {
+        watchState: true,
+    },
+});
